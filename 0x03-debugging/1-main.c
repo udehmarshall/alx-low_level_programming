dbbdeab5145e00d6260246c7292d21e1b118408b
udehmarshall <udehmarshall@gmail.com>
2023-06-22 03:04:19 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,6 +6,7 @@
 */
 
 
+
 int main(void)
 {
 
@@ -15,7 +16,7 @@ int main(void)
 	i = 0;
 	/*while (i < 10)*/
 	/*{*/
-	/*putchar(i);*/
+	/*	putchar(i);*/
 	/*}*/
 	printf("Infinite loop avoided! \\o/\n");
 	return (0);
